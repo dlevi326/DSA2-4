989e474172eb384d0fc85550eff7503bfb54da74
David Levi <dlevi623@gmail.com>
2017-12-06 18:41:41 -0500

    Cleaned up and added output file

--- dsa2proj4.cpp
+++ dsa2proj4.cpp
@@ -33,7 +33,7 @@ void printarr(int size1, int size2){
 
 }
 
-bool checkWord5(string s1,string s2, string s3){
+string checkWord(string s1,string s2, string s3){
 
 	dynam[0][0]=1;
 	for(int i=0;i<=s1.length();i++){
@@ -59,7 +59,7 @@ bool checkWord5(string s1,string s2, string s3){
 			}
 		}
 	}
-	printarr(s1.length(),s2.length());
+	//printarr(s1.length(),s2.length());
 
 	string newword = s3;
 
@@ -95,149 +95,41 @@ bool checkWord5(string s1,string s2, string s3){
 		}
 	}
 	else{
-		return false;
+		return "*** NOT A MERGE ***\n";
 	}
-	cout<<newword<<endl;
+	
 
-	return true;
+	return newword+"\n";
 
 }
-
-
-bool checkWord4(std::string s1, std::string s2, std::string s3){
-	//if (s1.length() + s2.length() != s3.length()) {
-	//	return false;
-	//}
-
-	for(int i=0;i<1001;i++){
-		for(int j=0;j<1001;j++){
-			dynam[i][j] = 0;
-		}
-	}
-
-	// s1 is 1
-	// s2 is 2
-	dynam[0][0] = 1;
-
-	for(int i=0;i<=s2.length();i++){
-		for(int j=0;j<=s1.length();j++){
-			if(!dynam[j][i]){
-				continue;
-			}
-
-			if(s1[j] == s3[i+j] && !dynam[j+1][i]){
-				dynam[j+1][i] = 1;
-			}
-
-			if(s2[i] == s3[i+j]  && !dynam[j][i+1]){
-				dynam[j][i+1] = 2;
-			}
-		}
-	}
-
-	int s1len = s1.length(), s2len = s2.length();
-	std::string newword = s3;
-
-	while(s1len+s2len>0){
-		if(!dynam[s1len][s2len]){
-			return false;
-		}
-
-		int ind = s1len+s2len-1;
-		if(dynam[s1len][s2len]==1){
-			newword[ind] = std::toupper(s3[ind]);
-		}
-
-		if(s2len > 0 && dynam[s1len][s2len-1]){
-			s2len--;
-		}
-		else if(s1len > 0 && dynam[s1len-1][s2len]){
-			s1len--;
-		}
-	}
-
-	std::cout<<newword<<std::endl;
-	return true;
-}
-
-bool checkWord3(std::string s1, std::string s2, std::string * s) {
-	if (s1.length() + s2.length() != s->length()) {
-		return false;
-	}
-
-	// Reset M
-	for (int n = 0; n < 1001; ++n) {
-		for (int m = 0; m < 1001; ++m) {
-			dynam[n][m] = 0;
-		}
-	}
-	dynam[0][0] = 1;
-
-	// Fill out M
-	int s1Char = 1, s2Char = 2;
-	for (int j = 0; (unsigned) j <= s2.length(); ++j) {
-		for (int i = 0; (unsigned) i <= s1.length(); ++i) {
-			if (!dynam[i][j]) {
-				continue;
-			}
-			if (s1[i] == (*s)[i + j] && !dynam[i + 1][j]) {
-				dynam[i + 1][j] = s1Char;
-			}
-			if (s2[j] == (*s)[i + j] && !dynam[i][j + 1]) {
-				dynam[i][j + 1] = s2Char;
-			}
-		}
-	}
-
-	printarr(s2.length()+1,s1.length()+1);
-
-	// Walk up matrix to see if s is a valid merge
-	int i = s1.length(), j = s2.length(), index;
-	while (i + j > 0) {
-		// Check failed case
-		if (!dynam[i][j]) {
-			return false;
-		}
-
-		index = i + j - 1;
-		if (dynam[i][j] == s1Char) {
-			(*s)[index] -= 32; // Convert to uppercase if from s1
-		}
-
-		// Prioritize first string
-		if (j > 0 && dynam[i][j - 1]) {
-			--j;
-		} else if (i > 0 && dynam[i - 1][j]) {
-			--i;
-		}
-	}
-
-	return true;
-}
-
 int main(){
 
-	std::cout<<"Enter name of input file: ";
-	std::string filein;
-	std::cin>>filein;
+	cout<<"Enter name of input file: ";
+	string filein;
+	cin>>filein;
+	ifstream file;
+    file.open(filein);
+    if (!file.is_open()){
+    	cout<<"ERROR"<<endl;
+    }
+	
 
-	std::ifstream file;
-	file.open(filein);
-	if (!file.is_open()){
-    	std::cout<<"ERROR"<<std::endl;
-    } 
+    string fileout;
+    cout<<"Enter output file: ";
+    cin>>fileout;
+    ofstream file2;
+    file2.open(fileout);
+    if (!file2.is_open()){
+    	cout<<"ERROR"<<endl;
+    }
+
 
     //string line;
     std::string word1,word2,word3;
     while(getline(file,word1)){
     	getline(file,word2);
     	getline(file,word3);
-    	if(checkWord5(word1,word2,word3)){
-			//std::cout<<word3<<std::endl;
-		}
-		else{
-			std::cout<<"** Not a Word **"<<std::endl;
-		}
+    	file2<<checkWord(word1,word2,word3);
     	
     }
 
